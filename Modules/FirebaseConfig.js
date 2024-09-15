4874431dd9d6d4a9660a2ed6395dab08be6c5399
PARASMANIKHUNTE <137080546+PARASMANI-KHUNTE@users.noreply.github.com>
2024-09-15 14:33:25 +0530

    Update FirebaseConfig.js

--- Modules/FirebaseConfig.js
+++ Modules/FirebaseConfig.js
@@ -8,7 +8,7 @@ const serviceAccount = require(serviceAccountPath);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET || 'ggvians-2c0ed.appspot.com',
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET ,
 });
 
 const bucket = admin.storage().bucket();
