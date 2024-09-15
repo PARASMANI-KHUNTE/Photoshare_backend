314df761ea02251e3f2a9e7c9f2a122d44bd7d1c
PARASMANIKHUNTE <137080546+PARASMANI-KHUNTE@users.noreply.github.com>
2024-09-15 14:14:54 +0530

    Update FirebaseConfig.js

--- Modules/FirebaseConfig.js
+++ Modules/FirebaseConfig.js
@@ -18,8 +18,10 @@ dotenv.config();
 // const storage = firebase.storage();
 // module.exports = storage;
 const admin = require('firebase-admin');
-const serviceAccount = require(`./${process.env.ggvians-2c0ed-firebase-adminsdk-a9cow-626cd394ae.json}`); // Download this file from Firebase Console
+const fs = require('fs');
+const serviceAccountPath = `./${process.env.ggvians-2c0ed-firebase-adminsdk-a9cow-626cd394ae.json}`; // Environment variable for the file path
 
+const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf-8'));
 // Initialize the Firebase Admin SDK
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
